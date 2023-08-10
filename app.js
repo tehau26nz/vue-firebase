fb7659f95b919b11ddb600c367dfa7de44a33c46
TNZ <92765882+tehau26nz@users.noreply.github.com>
2023-08-10 12:53:09 +1200

    Template App and Data

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const app = Vue.createApp({
     // Data or functions we want here
     // Template: '<h2>I am a template<h2>'
     // Best to keep this clear and create the template in the HTML file
+    data() {
+        return {
+            title: 'Tahitian Holiday',
+            author: 'Polynesian Writer',
+            age: '30'
+        }
+    }
 });
 
 app.mount('#app');

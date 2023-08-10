b068b6326c84fa353dc269e639219e252df99b3f
TNZ <92765882+tehau26nz@users.noreply.github.com>
2023-08-10 12:03:12 +1200

    Added the template and Vue app to HTML

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello, Vue');
+const app = Vue.createApp({
+    // Data or functions we want here
+    // Template: '<h2>I am a template<h2>'
+    // Best to keep this clear and create the template in the HTML file
+});
+
+app.mount('#app');
